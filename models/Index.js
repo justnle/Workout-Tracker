160ab0b8a283d44bbde089d865532c11a7e6e538
Justin <twoplatinumchainz@gmail.com>
2020-03-29 13:41:49 -0700

    Initial Index

---
+++ models/Index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const Cardio = require(`./Cardio`);
+const Resistance = require(`./Resistance`);
+const Workout = require(`./Workout`);
+
+module.exports = { Cardio, Resistance, Workout };
